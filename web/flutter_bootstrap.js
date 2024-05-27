14cc9b28309cadcadf31ebc9e8c60d6c35df19a8
Amos <amoshuke@qq.com>
2024-05-27 16:29:50 +0800

    Add: flutter_bootstrap.js

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,23 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+_flutter.loader.load({
+  config: {
+    assetBase: '/flutter_tilt_book/',
+    // https://github.com/flutter/flutter/issues/147610
+    // renderer: 'canvaskit',
+    canvasKitVariant: 'full',
+    canvasKitBaseUrl: '/flutter_tilt_book/canvaskit/'
+  },
+  serviceWorkerSettings: {
+    serviceWorkerVersion: {{flutter_service_worker_version}},
+  },
+  onEntrypointLoaded: async function (engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+    await appRunner.runApp();
+
+    // 清除加载
+    const amosLoader = document.getElementById('amos-loader-wrapper');
+    amosLoader?.remove();
+  },
+});
