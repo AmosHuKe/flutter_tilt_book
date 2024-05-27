15dca499bb99fca53a0db4fa3735426cefb6359a
Amos <amoshuke@qq.com>
2024-05-27 16:28:49 +0800

    Delete: main.js

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/widgets.dart';
 
-// import 'package:js/js.dart';
-
 import 'application.dart';
-
-// @JS("requestDeviceMotionEventPermission")
-// external void requestDeviceMotionEventPermission();
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  // requestDeviceMotionEventPermission();
   runApp(const Application());
 }
