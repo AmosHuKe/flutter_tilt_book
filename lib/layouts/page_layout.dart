262167aefea08cc4ba6c0d70604d0fbc0bf11f63
Amos <amoshuke@qq.com>
2024-08-09 11:17:35 +0800

    Fix: Transparent background

--- lib/layouts/page_layout.dart
+++ lib/layouts/page_layout.dart
@@ -39,10 +39,13 @@ class PageLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     final Layout layout = Layout(context);
 
-    return ClipRRect(
-      borderRadius: layout.sm
-          ? const BorderRadius.vertical(top: Radius.circular(24))
-          : BorderRadius.circular(24),
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFFF6F7FA),
+        borderRadius: layout.sm
+            ? const BorderRadius.vertical(top: Radius.circular(24))
+            : BorderRadius.circular(24),
+      ),
       child: LayoutAdaptive(
         mdChild: ListView(
           children: [
