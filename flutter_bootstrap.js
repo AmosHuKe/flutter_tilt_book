2aa73db3d3b56b20f8b9c4c1b924c5b85ab74a58
AmosHuKe <amoshuke@qq.com>
2024-08-14 09:01:30 +0000

    Deploying to pages-book from @ AmosHuKe/flutter_tilt_book@2dd2f37dcba7b4de1b76365d1fa38a7187778d9e 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -17,7 +17,7 @@ _flutter.loader.load({
     canvasKitBaseUrl: '/flutter_tilt_book/canvaskit/'
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "582826293",
+    serviceWorkerVersion: "262317972",
   },
   onEntrypointLoaded: async function (engineInitializer) {
     const appRunner = await engineInitializer.initializeEngine();
