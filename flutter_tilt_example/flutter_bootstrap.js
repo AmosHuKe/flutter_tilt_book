d280e9fb3e2f705c2ad59773d7dbeb32fcc9c1f0
AmosHuKe <amoshuke@qq.com>
2026-02-15 11:32:48 +0000

    Deploying to pages-book from @ AmosHuKe/flutter_tilt_book@9543159f1ccba01a926977839480c947d7ffaa7c 🚀

--- flutter_tilt_example/flutter_bootstrap.js
+++ flutter_tilt_example/flutter_bootstrap.js
@@ -43,7 +43,7 @@ _flutter.loader.load({
     canvasKitBaseUrl: '/flutter_tilt_book/flutter_tilt_example/canvaskit/'
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2283875385" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "3427222137" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
   onEntrypointLoaded: async function (engineInitializer) {
     const appRunner = await engineInitializer.initializeEngine();
